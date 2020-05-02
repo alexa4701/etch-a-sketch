783b0a3f761567d9aa3d75ab84d5975e16a10ca6
Alexa Coffin <alexacoffin1@gmail.com>
2020-05-02 12:17:41 -0500

    wip

--- js/main.js
+++ js/main.js
@@ -1,27 +1,37 @@
-let gridSize = 16;
-
-// Draws the board in the DOM.
-function drawBoard() {
-    let board = document.querySelector(".board");
-    
-    for(let rowNum = 0; rowNum < gridSize; rowNum++){
-      console.log(board);
-      // Create & append the row div
-      let rowDiv = document.createElement("div");
-      rowDiv.classList.add("board-row");
-      rowDiv.setAttribute("id", `row-${rowNum}`);
-
-      // Populate the row with cell divs
-      for(let cellNum = 0; cellNum < gridSize; cellNum++){
-        let cellDiv = document.createElement("div");
-        cellDiv.classList.add("board-cell");
-        cellDiv.setAttribute("id", `cell-${cellNum}`);
-        rowDiv.appendChild(cellDiv);
-      }
-
-      // Append the row div to the board
-      board.appendChild(rowDiv);
-    }
-}
+let gridSize = 100;
 
 drawBoard();
+
+// Adds listeners to the squares in the grid.
+const squaresDOM = document.querySelectorAll(".board-square");
+squaresDOM.forEach(square => {
+  square.addEventListener("mouseover", updateSquare);
+});
+
+// Creates the grid in the DOM.
+function drawBoard() {
+  let board = document.querySelector(".board");
+  
+  for(let rowNum = 0; rowNum < gridSize; rowNum++){
+    // Create & append the row div
+    let rowDiv = document.createElement("div");
+    rowDiv.classList.add("board-row");
+    rowDiv.setAttribute("id", `row-${rowNum}`);
+
+    // Populate the row with square divs
+    for(let squareNum = 0; squareNum < gridSize; squareNum++){
+      let squareDiv = document.createElement("div");
+      squareDiv.classList.add("board-square");
+      squareDiv.setAttribute("id", `square-${squareNum}`);
+      rowDiv.appendChild(squareDiv);
+    }
+
+    // Append the row div to the board
+    board.appendChild(rowDiv);
+  }
+}
+
+// Fills in the target square
+function updateSquare(event) {
+  event.target.style.backgroundColor = "blue";  
+}
